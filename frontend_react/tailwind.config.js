99b2c1e5edfd401030a8d57901e79b5538be0352
TangerineSpecter <993033472@qq.com>
2025-12-12 11:02:16 +0800

    feat：新增自定义字体

--- frontend_react/tailwind.config.js
+++ frontend_react/tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // 自定义 sans 字体栈
+        sans: [
+          'MyCustomFont',
+          'MiSans',
+          'Noto Sans SC',
+          'LXGW WenKai GB Screen',
+          '"Noto Sans SC"',
+          '"PingFang SC"',
+          '"Microsoft YaHei"',
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif'
+        ],
+        // 自定义 mono 代码字体
+        mono: [
+          '"JetBrains Mono"',
+          'Nunito',
+          '"Fira Code"',
+          'ui-monospace',
+          'SFMono-Regular',
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          'monospace'
+        ],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
